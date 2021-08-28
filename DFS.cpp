509177a838830da059e7a8e881bcf29e70fbf47a
Apeksha Jadeja <81768737+Apeksha-Jadeja@users.noreply.github.com>
2021-08-28 15:10:50 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,70 +1,71 @@
-#include<iostream> 
-#include<list> 
+#include<bits/stdc++.h>
 using namespace std;
 
 class Graph
 {
-    int V;    
-    list<int>* adj;
-
-   
-    void DFSUtil(int v, bool visited[]);
+    int V; 
+    list<int> *adj;
 public:
-    Graph(int V);   
-    void addEdge(int v, int w);
-
-  
-    void DFS(int v);
+    Graph(int V);  
+    void addEdge(int v, int w); 
+    void DFS(int s); 
+   
 };
-
+ 
 Graph::Graph(int V)
 {
     this->V = V;
     adj = new list<int>[V];
 }
-
+ 
 void Graph::addEdge(int v, int w)
 {
-    adj[v].push_back(w); 
 }
+ 
 
-void Graph::DFSUtil(int v, bool visited[])
+void Graph::DFS(int s)
 {
+    
+    vector<bool> visited(V, false);
+ 
+ 
+    stack<int> stack;
+ 
    
-    visited[v] = true;
-    cout << v << " ";
-
-  
-    list<int>::iterator i;
-    for (i = adj[v].begin(); i != adj[v].end(); ++i)
-        if (!visited[*i])
-            DFSUtil(*i, visited);
+    stack.push(s);
+ 
+    while (!stack.empty())
+    {
+    
+        int s = stack.top();
+        stack.pop();
+ 
+     
+        if (!visited[s])
+        {
+            cout << s << " ";
+            visited[s] = true;
+        }
+ 
+       
+        for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
+            if (!visited[*i])
+                stack.push(*i);
+    }
 }
-
-
-void Graph::DFS(int v)
-{
-
-    bool* visited = new bool[V];
-    for (int i = 0; i < V; i++)
-        visited[i] = false;
-
-    DFSUtil(v, visited);
-}
+ 
 
 int main()
-{
-   
-    Graph g(4);
-    g.addEdge(0, 1);
+
+    Graph g(5); 
+    g.addEdge(1, 0);
     g.addEdge(0, 2);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(3, 3);
-
-    cout << "Following is Depth First Traversal (starting from vertex 2)"; 
-        g.DFS(2);
-
+    g.addEdge(2, 1);
+    g.addEdge(0, 3);
+    g.addEdge(1, 4);
+ 
+    cout << "Following is Depth First Traversal\n";
+    g.DFS(0);
+ 
     return 0;
 }
